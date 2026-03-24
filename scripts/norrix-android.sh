7ce8c22e98b63d2cefe2d6508342eddd864dc502
Owen Carey <37121709+owenthcarey@users.noreply.github.com>
2026-03-23 21:44:00 -0700

    chore: add scripts for Norrix builds and emulator runs

---
+++ scripts/norrix-android.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build Android release with Norrix
+norrix build android release --dev
+
+# Submit to Google Play with Norrix
+# norrix submit android --dev
---
+++ scripts/run-emulators.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Phone screenshots
+ns run android --device Medium_Phone_API_31
+
+# 7-inch tablet screenshots
+# ns run android --device Nexus_7_API_31
+
+# 10-inch tablet screenshots
+# ns run android --device Pixel_C_API_31
